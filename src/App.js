40ccb3562aab79e9b8eae97945353830fb78e7e4
ChoiPureum <poo1994@hanmail.net>
2020-07-18 10:23:43 +0900

    Movie API install

--- src/App.js
+++ src/App.js
@@ -1,45 +1,31 @@
 import React from 'react';
 import ProtoTypes from 'prop-types';
+import axios from 'axios';
 
-// 클래스형 컴포넌트
-    //반환을 위해 return이 아닌 render()를 사용해야한다 
 class App extends React.Component{
-    constructor(props){
-        super(props);
-        console.log('hello')
-    }
     state={
-        count:0,
+        isLoading:true,
+        movie:[]
+        
     };
-    //state를 건드리는건 성능문제가 생길 수 있다
-        // current map형식으로 
-    add =() =>{
-        this.setState(current=>({
-            count: current.count+1
-        }));
-    }
-    minus=() =>{
-        this.setState(current=>({
-            count: current.count-1
-        }));
+    getMovies=async()=>{
+        const {
+            data:{
+                data:{movies}
+            }
+        } = await axios.get('https://yts-proxy.now.sh/list_movies.json');
+        this.setState({movies,isLoading:false});
     }
     componentDidMount(){
-        //render이후 실행되는 영역
-    }
-    componentDidUpdate(){
-        //render가 실행되면 실행
-    }
-    componentWillUnmount(){
-        //컴포넌트가 화면ㅇ에서 떠날때 실행
+      this.getMovies();
     }
     render(){
+        const{isLoading} =this.state;
         return(
-            <div>       
-                <h1>The number is: {this.state.count}</h1>
-                <button onClick={this.add}>Add</button>
-                <button onClick={this.minus}>Minus</button>
+        <div>
+            {isLoading?'Loading':'We are Ready'}
         </div>
-        ) 
+        )
     }
     
 }
---
+++ src/Movies.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Movie(){
+    return <h1></h1>
+
+}
+Movie.prototype={};
+
+export default Movie;
